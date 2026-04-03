3bfdd2f9f511005c5185d179896ddccf02d4a540
dat_dude_david <davkpayi@gmail.com>
2026-04-03 22:38:04 +0000

    made the formatting look better: str::utils.foo()

--- strutils.hpp
+++ strutils.hpp
@@ -8,13 +8,20 @@
 #include <algorithm> // For ranges
 #include <cctype>    // For toupper
 
-struct strutils{
+namespace str{
+    struct utils;
+}
 
-    // 1. The Housekeeper: Removes spaces from the string
-    // 2. The Bread Knife: Slices a string into a list (vector)
-    // 3. The Shouter: Converts text to UPPERCASE using Modern Ranges
-    // 4. The Scout: Checks if a word is hidden inside another                                                                                                                                                                                                          
-    // 5. The Glue: Joins a list of words with a separator (like a dash)
-    // Bonus for v0.2: Replace all instances of a word
+struct str::utils{
 
+// str::util.trim(string) Removes spaces from the string
+utils& trim(std::string& s){
+    s.erase(std::remove_if(s.begin(), s.end(), ::isspace), s.end());
+    return *this;
+}
+// 2. The Bread Knife: Slices a string into a container (vector)
+// 3. The Shouter: Converts text to UPPERCASE using Modern Ranges
+// 4. The Scout: Checks if a word is hidden inside another
+// 5. The Glue: Joins a list of words with a separator (like a dash)
+// Bonus for v0.2: Replace all instances of a word
 };
