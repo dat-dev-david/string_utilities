74a8dc70ad7341cce74e2afb96a831c536c1dddf
dat_dude_david <davkpayi@gmail.com>
2026-04-04 17:51:03 +0000

    made the join function to join vector elements together

--- strutils.hpp
+++ strutils.hpp
@@ -3,7 +3,6 @@
 #endif
 
 #include <string>
-#include <string_view>
 #include <vector>
 #include <algorithm> // For ranges
 #include <cctype>    // For toupper
@@ -14,24 +13,30 @@ namespace str{
 
 struct str::utils{
 
-// str::util.trim(string) Removes spaces from the string
+// str::util.trim(string) removes spaces from the string
    utils& trim(std::string& s){
        s.erase(std::remove_if(s.begin(), s.end(), ::isspace), s.end());
        return *this;
    }
-// str::utils.split breaks a string into a vector without 
-   utils& split(std::string& s, std::vector<std::string>& container, char whitespace){
-       size_t string_whitespace{s.find(whitespace)};
+// str::utils.split(string, vector) breaks a string into a vector without 
+   utils& split(std::string& s, std::vector<std::string>& container){
+       size_t string_whitespace{s.find(' ')};
        size_t pos{0};
        while(string_whitespace != s.length()){
-          container.push_back((s.substr(pos, string_whitespace - pos)));
-          pos{string_whitespace + 1};
-          string_whitespace{pos};
+          container.push_back(std::string(s.substr(pos, string_whitespace - pos)));
+          pos = string_whitespace + 1;
+          string_whitespace = pos;
        }
        return *this;
    }
-// 3. The Shouter: Converts text to UPPERCASE using Modern Ranges
-// 4. The Scout: Checks if a word is hidden inside another
+//str::utils.uppercase(string) - converts text to UPPERCASE using ::toupper
+    utils& uppercase(std::string& s){
+       std::transform(s.begin(), s.end(), s.begin(), ::toupper);
+       return *this;
+ }
 // 5. The Glue: Joins a list of words with a separator (like a dash)
+    utils& bond(std::vector<std::string>& words, char sperator){
+        std::string s;
+    }
 // Bonus for v0.2: Replace all instances of a word
 };
